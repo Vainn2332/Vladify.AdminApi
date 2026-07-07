777ce47f26441be70cbebe1858cba9ea30e86003
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-07 14:04:24 +0200

    add moderationTask

---
+++ Vladify.Domain/Entities/ModerationTask.cs
@@ -0,0 +1,20 @@
+﻿using Vladify.Domain.Enums;
+
+namespace Vladify.Domain.Entities;
+
+public class ModerationTask
+{
+    public Guid Id { get; set; }
+
+    public Guid SongId { get; set; }
+
+    public Guid AssignedModeratorId { get; set; }
+
+    public Status Status { get; set; }
+
+    public string? Message { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public DateTimeOffset? ResolvedAt { get; set; }
+}
