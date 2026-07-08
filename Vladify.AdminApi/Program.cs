6d886029e50b28e01b78b740eb8b2c9d87f34cfc
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-08 15:39:16 +0200

    update program

--- Vladify.AdminApi/Program.cs
+++ Vladify.AdminApi/Program.cs
@@ -1,5 +1,6 @@
 using Vladify.AdminApi.Extensions;
 using Vladify.Application.Extensions;
+using Vladify.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddApiServices();
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
