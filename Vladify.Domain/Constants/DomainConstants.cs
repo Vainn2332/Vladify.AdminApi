abd0a6a2320f6035d9a99e75f6c7bb9016994640
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-08 15:17:05 +0200

    add domain constants class

---
+++ Vladify.Domain/Constants/DomainConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Vladify.Domain.Constants;
+
+public static class DomainConstants
+{
+    public static class ModerationTask
+    {
+        public const int MaxMessageLength = 250;
+    }
+}
