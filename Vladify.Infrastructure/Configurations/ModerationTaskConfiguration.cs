d1b8dea24e9209c231fcd10ecc390946ce001c5e
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-08 15:17:20 +0200

    add moderation task configuration

---
+++ Vladify.Infrastructure/Configurations/ModerationTaskConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vladify.Domain.Constants;
+using Vladify.Domain.Entities;
+
+namespace Vladify.Infrastructure.Configurations;
+
+public class ModerationTaskConfiguration : IEntityTypeConfiguration<ModerationTask>
+{
+    public void Configure(EntityTypeBuilder<ModerationTask> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Status).HasConversion<string>();
+        builder.HasIndex(p => p.SongId).IsUnique();
+        builder.Property(p => p.Message).HasMaxLength(DomainConstants.ModerationTask.MaxMessageLength);
+    }
+}
