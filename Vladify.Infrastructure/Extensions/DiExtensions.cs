0055f010f40aa7aa4af4b5682daf094bb6cd6915
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-08 15:37:08 +0200

    add to di

---
+++ Vladify.Infrastructure/Extensions/DiExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Vladify.Application.Exceptions;
+
+namespace Vladify.Infrastructure.Extensions;
+
+public static class DiExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddInfrastructure(IConfiguration configuration)
+        {
+            return services
+                .AddPostgresDb(configuration);
+        }
+
+        public IServiceCollection AddPostgresDb(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("ApplicationDbContext")
+                ?? throw new NotFoundException("Connection string for dbContext not found!");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+
+            return services;
+        }
+
+    }
+}
