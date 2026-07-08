9c0c6fbacbf36ea41de6de300f2b1389bf16bac2
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-08 12:47:47 +0200

    register options in DI

---
+++ Vladify.Application/Extensions/DiExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Vladify.Application.Options;
+
+namespace Vladify.Application.Extensions;
+
+public static class DiExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddOptions()
+        {
+            services
+                .AddOptions<Auth0Options>()
+                .BindConfiguration(Auth0Options.SectionName)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            return services;
+        }
+    }
+}
