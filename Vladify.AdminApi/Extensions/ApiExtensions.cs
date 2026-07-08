4e0540d6c94ffab60afe70ad28cdd2e44f5f7449
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-08 13:30:42 +0200

    add auth

---
+++ Vladify.AdminApi/Extensions/ApiExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Vladify.Application.Options;
+
+namespace Vladify.AdminApi.Extensions;
+
+public static class ApiExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddAppServices()
+        {
+            return services
+                .AddJwtBasedAuthentication()
+                .AddPolicyBasedAuthorization();
+        }
+
+        public IServiceCollection AddJwtBasedAuthentication()
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+
+            services.AddOptions<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme)
+            .Configure<IOptions<Auth0Options>>((options, auth0) =>
+            {
+                var auth0Options = auth0.Value;
+
+                options.Authority = auth0Options.Authority;
+                options.Audience = auth0Options.Audience;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+            return services;
+        }
+
+        public IServiceCollection AddPolicyBasedAuthorization()
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminOrModerator", policy =>
+                {
+                    policy.RequireClaim("https://my-app.com/roles", "Admin", "Moderator");
+                });
+            });
+
+            return services;
+        }
+    }
+}
