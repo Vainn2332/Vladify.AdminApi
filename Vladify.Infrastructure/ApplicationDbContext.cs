7b035cdea8bfb073bfabfcb19aeda4e5631c839f
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-08 15:19:49 +0200

    apply configuration

---
+++ Vladify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vladify.Domain.Entities;
+using Vladify.Infrastructure.Configurations;
+
+namespace Vladify.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<ModerationTask> ModerationTasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ModerationTaskConfiguration());
+        base.OnModelCreating(modelBuilder);
+    }
+}
