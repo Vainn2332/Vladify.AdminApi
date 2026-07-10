b921aabe069d014c2ff1d9e75a2e257d415485eb
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-10 15:24:29 +0200

    add constants

---
+++ Vladify.Infrastructure/Constants/PollyPipelineConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Vladify.Infrastructure.Constants;
+
+public static class PollyPipelineConstants
+{
+    public const string DbRetryPipelineName = "DbRetryPipeline";
+
+    public const int MaxAmountOfRetries = 3;
+
+    public static readonly TimeSpan Delay = TimeSpan.FromSeconds(1);
+}
