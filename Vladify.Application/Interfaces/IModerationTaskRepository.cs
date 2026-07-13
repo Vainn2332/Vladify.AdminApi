737c5194c44a0302cd51714465acc2a31e358c17
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-13 13:33:37 +0200

    add repository interface

---
+++ Vladify.Application/Interfaces/IModerationTaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using Vladify.Application.Models;
+using Vladify.Domain.Entities;
+
+namespace Vladify.Application.Interfaces;
+
+public interface IModerationTaskRepository
+{
+    public Task<ModerationTask> CreateAsync(ModerationTask task, CancellationToken cancellationToken);
+
+    public List<ModerationTask> GetAllAsync(PaginationFilter paginationFilter, CancellationToken cancellationToken);
+
+    public ModerationTask GetAsync(Guid id, CancellationToken cancellationToken);
+
+    public ModerationTask UpdateAsync(ModerationTask task, CancellationToken cancellationToken);
+
+    public ModerationTask DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
