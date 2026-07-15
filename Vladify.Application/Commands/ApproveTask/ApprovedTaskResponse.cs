2242e5fe09073a7911ef039dfd15305c5cdf1cf4
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-15 14:36:56 +0200

    update approved task response to inline record

--- Vladify.Application/Commands/ApproveTask/ApprovedTaskResponse.cs
+++ Vladify.Application/Commands/ApproveTask/ApprovedTaskResponse.cs
@@ -1,8 +1,3 @@
 ﻿namespace Vladify.Application.Commands.ApproveTask;
 
-public record ApprovedTaskResponse
-{
-    public Guid Id { get; set; }
-
-    public DateTimeOffset? ResolvedAt { get; set; }
-}
+public record ApprovedTaskResponse(Guid Id, DateTimeOffset ResolvedAt);
