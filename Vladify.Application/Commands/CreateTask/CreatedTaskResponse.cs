79e3284e55023341c1f46354da97fc2968f3e100
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-15 12:56:19 +0200

    create response dto

---
+++ Vladify.Application/Commands/CreateTask/CreatedTaskResponse.cs
@@ -0,0 +1,14 @@
+﻿using Vladify.Domain.Enums;
+
+namespace Vladify.Application.Commands.CreateTask;
+
+public record CreatedTaskResponse
+{
+    public Guid Id { get; set; }
+
+    public Guid SongId { get; set; }
+
+    public ModerationStatus Status { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+}
