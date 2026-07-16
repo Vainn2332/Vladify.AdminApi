c124de5c27a3d0e6e98415bf84f27455c3f08b33
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-16 14:52:38 +0200

    add AssingTask command tests

---
+++ Vladify.Tests/UnitTests/ModerationTasks/Commands/AssignTask/AssignTaskCommandTests.cs
@@ -0,0 +1,56 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Moq;
+using Vladify.Application.Commands.ModerationTask.AssignTask;
+using Vladify.Application.Interfaces;
+
+namespace Vladify.Tests.UnitTests.ModerationTasks.Commands.AssignTask;
+
+public class AssignTaskCommandTests
+{
+    private readonly IFixture _fixture;
+    private readonly Mock<IModerationTaskRepository> _repositoryMock;
+    private readonly AssignTaskCommandHandler _handler;
+
+    public AssignTaskCommandTests()
+    {
+        _fixture = new Fixture().Customize(new AutoMoqCustomization());
+        _repositoryMock = _fixture.Freeze<Mock<IModerationTaskRepository>>();
+        _handler = _fixture.Create<AssignTaskCommandHandler>();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnTaskId_WhenTaskIsSuccessfullyClaimed()
+    {
+        var command = _fixture.Create<AssignTaskCommand>();
+        var expectedTaskId = _fixture.Create<Guid>();
+
+        _repositoryMock
+            .Setup(repo => repo.ClaimNextPendingTaskAsync(command.ModeratorId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedTaskId);
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        result.Should().NotBeNull()
+            .And.Be(expectedTaskId);
+
+        _repositoryMock.Verify(repo => repo.ClaimNextPendingTaskAsync(command.ModeratorId, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnNull_WhenNoPendingTasksInQueue()
+    {
+        var command = _fixture.Create<AssignTaskCommand>();
+
+        _repositoryMock
+            .Setup(repo => repo.ClaimNextPendingTaskAsync(command.ModeratorId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Guid?)null);
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        result.Should().BeNull();
+
+        _repositoryMock.Verify(repo => repo.ClaimNextPendingTaskAsync(command.ModeratorId, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
