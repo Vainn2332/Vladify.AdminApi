f2c98a7c4f6f8ff39b8ae1329d8e2196ce5225de
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-17 14:01:26 +0200

    update response dto

--- Vladify.Application/Commands/ModerationTasks/RejectTask/RejectedTaskResponse.cs
+++ Vladify.Application/Commands/ModerationTasks/RejectTask/RejectedTaskResponse.cs
@@ -1,3 +1,3 @@
 ﻿namespace Vladify.Application.Commands.ModerationTasks.RejectTask;
 
-public record RejectedTaskResponse(Guid Id, DateTimeOffset ResolvedAt);
+public record RejectedTaskResponse(Guid Id, DateTimeOffset ResolvedAt, string Message);
