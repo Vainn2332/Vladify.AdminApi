1be29316ab54bffe0ec04ca12f051304e0625141
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-27 15:06:52 +0200

    add task controller

---
+++ Vladify.AdminApi/Controllers/TasksController.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Vladify.AdminApi.Constants;
+using Vladify.AdminApi.Extensions;
+using Vladify.Application.Commands.ModerationTasks.ApproveTask;
+using Vladify.Application.Commands.ModerationTasks.AssignTask;
+using Vladify.Application.Commands.ModerationTasks.RejectTask;
+
+namespace Vladify.AdminApi.Controllers
+{
+    [Route("api/tasks")]
+    [ApiController]
+    [Authorize(Policy = AuthPolicies.AdminOrModerator)]
+    public class TasksController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("assign")]
+        public Task<Guid?> AsssignTask()
+        {
+            var moderatorId = User.GetAuth0Id();
+            var command = new AssignTaskCommand(moderatorId);
+
+            return mediator.Send(command);
+        }
+
+        [HttpPut("{id}/approve")]
+        public Task<ApprovedTaskResponse> Approve(Guid id)
+        {
+            var moderatorId = User.GetAuth0Id();
+
+            var command = new ApproveTaskCommand(id, moderatorId);
+
+            return mediator.Send(command);
+        }
+
+        [HttpPut("{id}/reject")]
+        public Task<RejectedTaskResponse> Reject(Guid id)
+        {
+            var moderatorId = User.GetAuth0Id();
+
+            var command = new RejectTaskCommand(id, moderatorId);
+
+            return mediator.Send(command);
+        }
+    }
+}
