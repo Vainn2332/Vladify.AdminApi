2da0106a3e8f403b93090828ad2ea3292cc476d4
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-28 13:59:45 +0300

    add grpc service

---
+++ Vladify.AdminApi/Grpc/ModerationTask/ModerationGrpcService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using MediatR;
+using Vladify.Application.Commands.ModerationTasks.CreateTask;
+using Vladify.GrpcContracts;
+
+namespace Vladify.AdminApi.Grpc.ModerationTask;
+
+public class ModerationGrpcService(IMediator mediator) : ModerationGrpc.ModerationGrpcBase
+{
+    public async override Task<CreateTaskResponse> CreateTask(CreateTaskRequest request, ServerCallContext context)
+    {
+        var songId = Guid.Parse(request.SongId);
+        var command = new CreateTaskCommand(songId);
+
+        var response = await mediator.Send(command, context.CancellationToken);
+
+        return new CreateTaskResponse()
+        {
+            TaskId = response.Id.ToString()
+        };
+    }
+}
