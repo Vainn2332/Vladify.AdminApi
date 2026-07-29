1183ef0db0d08452442d18a5809ee3c931cdfc24
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-29 15:42:12 +0200

    add migration

---
+++ Vladify.Infrastructure/Migrations/20260729134120_ChangeModeratorId_ToStringType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vladify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeModeratorId_ToStringType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AssignedModeratorId",
+                table: "ModerationTasks",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AssignedModeratorId",
+                table: "ModerationTasks",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
